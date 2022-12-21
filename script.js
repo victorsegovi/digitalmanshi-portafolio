a38edb55043c6893209ac41ec54529752e680a9b
vic <victoralejandrost881@gmail.com>
2022-12-21 12:47:12 -0430

    Added two (2) new videos and corrected some accesibility issues

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const close = document.getElementById("close");
 const sections = document.getElementById("sections");
 const main = document.getElementById('home');
 const body = document.getElementById('body');
+const videoChefWeb = document.getElementById('video-chef-web');
+const videoCirujanoWeb = document.getElementById('video-cirujano-web')
+const videoChefSafari = document.getElementById('video-chef-safari');
+const videoCirujanoSafari = document.getElementById('video-cirujano-safari');
 
 
 if(body.offsetWidth <= 599){
@@ -21,4 +25,12 @@ if(body.offsetWidth <= 599){
       }
       sections.addEventListener('click', closing)
       main.addEventListener('click', closing)
+}
+
+if(platform.name == 'Safari' || platform.name == 'Chrome Mobile'){
+    videoChefWeb.style.display = 'none';
+    videoCirujanoWeb.style.display = 'none';
+
+    videoChefSafari.style.display = 'block';
+    videoCirujanoSafari.style.display = 'block';
 }
